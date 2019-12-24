a162e5a5d30a6caf8af0017a1c7037d340d83466
Quentin Wright <wrightq00gmail.com>
2019-12-23 22:12:54 -0600

    change embed style

--- dist/commands/stats/info.js
+++ dist/commands/stats/info.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const discord_akairo_1 = require("discord-akairo");
 const discord_js_1 = require("discord.js");
 const Stats_1 = require("../../structures/models/Stats");
+const common_tags_1 = require("common-tags");
 class InfoCommand extends discord_akairo_1.Command {
     constructor() {
         super('info', {
@@ -12,7 +13,6 @@ class InfoCommand extends discord_akairo_1.Command {
                 usage: '<method>'
             },
             category: 'stats',
-            channel: 'guild',
             ratelimit: 2,
             args: [
                 {
@@ -23,23 +23,37 @@ class InfoCommand extends discord_akairo_1.Command {
         });
     }
     async exec(message, { type }) {
+        const capitalize = (str) => str.split('').map((v, i) => i === 0 ? v.toUpperCase() : v.toLowerCase()).join('');
         if (type === 'global') {
-            let stats = await Stats_1.Stats.findOne({ id: '0' }).lean();
-            let embed = new discord_js_1.MessageEmbed()
+            const stats = await Stats_1.Stats.findOne({ id: '0' });
+            const most = [...stats.commands.entries()].sort((a, b) => b[1] - a[1])[0];
+            const total = [...stats.commands.values()].reduce((p, c) => p + c, 0);
+            const embed = new discord_js_1.MessageEmbed()
                 .setColor([155, 200, 200])
                 .setTitle('**Global Statistics**')
-                .addField('Total Messages', stats.messages, true)
-                .setFooter('Global Stats')
+                .setDescription(common_tags_1.stripIndents `\`\`\`js
+                    Total Messages: ${stats.messages}
+                    Total Command Usage: ${total}
+                    Most Used Command: ${capitalize(most[0])} with ${most[1]} uses
+                \`\`\``)
                 .setTimestamp(Date.now());
             return message.util.send(embed);
         }
         else if (type === 'guild') {
-            let stats = await Stats_1.Stats.findOne({ id: message.guild.id }).lean();
-            let embed = new discord_js_1.MessageEmbed()
+            if (!message.guild)
+                return message.util.send('This can only be used in a guild.').then(m => this.client.delete(message, m, 3000));
+            const stats = await Stats_1.Stats.findOne({ id: message.guild.id });
+            let most = [...stats.commands.entries()].sort((a, b) => b[1] - a[1])[0];
+            const total = [...stats.commands.values()].reduce((p, c) => p + c, 0);
+            const embed = new discord_js_1.MessageEmbed()
                 .setColor([155, 200, 200])
                 .setTitle('**Guild Statistics**')
-                .addField('Total Messages', stats.messages, true)
-                .setFooter('Global Stats')
+                .setDescription(common_tags_1.stripIndents `\`\`\`js
+                    Total Messages: ${stats.messages}
+                    Total Command Usage: ${total}
+                    Most Used Command: ${capitalize(most[0])} with ${most[1]} uses
+                \`\`\``)
+                .setFooter('Guild Stats')
                 .setTimestamp(Date.now());
             return message.util.send(embed);
         }
--- src/bot/commands/stats/info.ts
+++ src/bot/commands/stats/info.ts
@@ -1,6 +1,7 @@
 import { Command } from 'discord-akairo';
 import { Message, MessageEmbed } from 'discord.js';
 import { Stats } from '../../structures/models/Stats';
+import { stripIndents } from 'common-tags';
 
 export default class InfoCommand extends Command {
     public constructor() {
@@ -11,7 +12,6 @@ export default class InfoCommand extends Command {
                 usage: '<method>'
             },
             category: 'stats',
-            channel: 'guild',
             ratelimit: 2,
             args: [
                 {
@@ -22,24 +22,36 @@ export default class InfoCommand extends Command {
         });
     }
 
-    public async exec(message: Message, { type }: { type: string }): Promise<Message | Message[]> {
+    public async exec(message: Message, { type }: { type: string }): Promise<Message | Message[] | void> {
+        const capitalize = (str: string): string => str.split('').map((v, i) => i === 0 ? v.toUpperCase() : v.toLowerCase()).join('');
         if (type === 'global') {
-            let stats = await Stats.findOne({ id: '0' }).lean();
-            let embed = new MessageEmbed()
+            const stats = await Stats.findOne({ id: '0' });
+            const most = [...stats!.commands.entries()].sort((a, b) => b[1] - a[1])[0];
+            const total = [...stats!.commands.values()].reduce((p, c) => p + c, 0);
+            const embed = new MessageEmbed()
                 .setColor([155, 200, 200])
                 .setTitle('**Global Statistics**')
-                .addField('Total Messages', stats!.messages, true)
-                .setFooter('Global Stats')
+                .setDescription(stripIndents`\`\`\`js
+                    Total Messages: ${stats!.messages}
+                    Total Command Usage: ${total}
+                    Most Used Command: ${capitalize(most[0])} with ${most[1]} uses
+                \`\`\``)
                 .setTimestamp(Date.now())
-
             return message.util!.send(embed);
         } else if (type === 'guild') {
-            let stats = await Stats.findOne({ id: message.guild!.id }).lean();
-            let embed = new MessageEmbed()
+            if (!message.guild) return message.util!.send('This can only be used in a guild.').then(m => this.client.delete(message, m, 3000));
+            const stats = await Stats.findOne({ id: message.guild!.id });
+            let most = [...stats!.commands.entries()].sort((a, b) => b[1] - a[1])[0];
+            const total = [...stats!.commands.values()].reduce((p, c) => p + c, 0);
+            const embed = new MessageEmbed()
                 .setColor([155, 200, 200])
                 .setTitle('**Guild Statistics**')
-                .addField('Total Messages', stats!.messages, true)
-                .setFooter('Global Stats')
+                .setDescription(stripIndents`\`\`\`js
+                    Total Messages: ${stats!.messages}
+                    Total Command Usage: ${total}
+                    Most Used Command: ${capitalize(most[0])} with ${most[1]} uses
+                \`\`\``)
+                .setFooter('Guild Stats')
                 .setTimestamp(Date.now())
 
             return message.util!.send(embed);
