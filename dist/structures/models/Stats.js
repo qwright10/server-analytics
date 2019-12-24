4f9b939f8b2149f9c6526ab63d76719f13b47f96
Quentin Wright <wrightq00gmail.com>
2019-12-23 22:13:48 -0600

    update stats model

--- dist/structures/models/Stats.js
+++ dist/structures/models/Stats.js
@@ -4,7 +4,15 @@ const mongoose_1 = require("mongoose");
 const StatsSchema = new mongoose_1.Schema({
     id: String,
     name: String,
+    commands: {
+        type: Map,
+        of: Number
+    },
     emojis: {
+        type: Map,
+        of: Number
+    },
+    reactions: {
         type: Map,
         of: Number
     },
--- src/bot/structures/models/Stats.ts
+++ src/bot/structures/models/Stats.ts
@@ -3,14 +3,24 @@ import { Document, Schema, model } from 'mongoose';
 export interface Stats extends Document {
     id: string,
     name: string,
-    emojis: Map<string, Number>,
+    commands: Map<string, number>,
+    emojis: Map<string, number>,
+    reactions: Map<string, number>,
     messages: number
 }
 
 const StatsSchema = new Schema({
     id: String,
     name: String,
+    commands: {
+        type: Map,
+        of: Number
+    },
     emojis: {
+        type: Map,
+        of: Number
+    },
+    reactions: {
         type: Map,
         of: Number
     },
